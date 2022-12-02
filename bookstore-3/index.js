7a9580ea7e4d0b89d40a2af942fd20289dc3f131
Ariyo Joseph Olamide <midejoseph24@gmail.com>
2022-12-02 14:36:19 +0100

    Add processor-3 code #03

---
+++ bookstore-3/index.js
@@ -0,0 +1,21 @@
+exports.handler = async (event) => {
+    /* log out the event body */
+    console.log(JSON.stringify(event));
+    
+    /* extract the order details from the sqs message */
+    const order = JSON.parse(event.Records[0].body);
+    
+    /*  log out the order */
+    console.log('Received Order:', order);
+    console.log('Book Shop #3 Retrieving Book from Store ....');
+    
+    /* wait for 5secs*/
+    await sleep(5000);
+};
+
+/* define sleep function*/
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    })
+}
